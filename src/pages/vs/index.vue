10b772b858c1b71e9c6e0a59942afd250d71ff36
Leonid Lavrov <l.lavrov@kari.com>
2026-05-14 11:51:10 +0500

    изменен адрес iframe для vibro

--- src/pages/vs/index.vue
+++ src/pages/vs/index.vue
@@ -1,6 +1,6 @@
 <template>
   <MainPageLayout :breadcrumbs="breadcrumbs" />
-  <iframe class="frame" src="https://vibro.constrtodo.ru"></iframe>
+  <iframe class="frame" src="https://vibro.constrtodo.ru:3445/#/vibro"></iframe>
 </template>
 
 <script setup>
